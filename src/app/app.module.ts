82792cfe72878bd6fcbba2036ea95028d1ae0235
bosecodes <somdevbasu100@gmail.com>
2025-01-10 13:33:56 +0530

    implementing ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { CounterbuttonComponent } from './component/counterbutton/counterbutton.component';
+import { counterReducer } from './shared/store/counter.reducer';
+import { CounterdisplayComponent } from './component/counterdisplay/counterdisplay.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/Material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterbuttonComponent,
+    CounterdisplayComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({counter: counterReducer}, {}),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
