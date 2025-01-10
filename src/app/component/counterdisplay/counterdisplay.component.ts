2778fd788b0eec35e39dd0d313882014a73ab8f4
bosecodes <somdevbasu100@gmail.com>
2025-01-10 18:01:37 +0530

    adding selectors

--- src/app/component/counterdisplay/counterdisplay.component.ts
+++ src/app/component/counterdisplay/counterdisplay.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { CounterModel } from 'src/app/shared/store/counter.model';
+import { getCounter } from 'src/app/shared/store/counter.selector';
 
 @Component({
   selector: 'app-counterdisplay',
@@ -12,21 +13,19 @@ export class CounterdisplayComponent implements OnInit, OnDestroy {
   constructor(private store: Store<{counter: CounterModel}>) {}
 
   counterDisplay!: number;
-  channelName = '';
 
   counterSubscribe !: Subscription;
   counter$ !: Observable<CounterModel>;
 
   ngOnInit(): void {
-    this.counterSubscribe = this.store.select('counter').subscribe(data => {
-      this.counterDisplay = data.counter;
-      this.channelName = data.channelName;
+    this.counterSubscribe = this.store.select(getCounter).subscribe(data => {
+      this.counterDisplay = data;
+      console.log('counter display');
     });
     // the above subscription can be omitted, and we can directly store the 
     // return value in an observable counter$
     // in this case we don't even have to unsubscribe onDestroy
 
-    this.counter$ = this.store.select('counter');
   }
 
   ngOnDestroy(): void {
--- src/app/customcounter/customcounter.component.ts
+++ src/app/customcounter/customcounter.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { customIncrement } from '../shared/store/counter.actions';
 import { CounterModel } from '../shared/store/counter.model';
+import { Observable, Subscription } from 'rxjs';
+import { getChannelName } from '../shared/store/counter.selector';
 
 @Component({
   selector: 'app-customcounter',
   templateUrl: './customcounter.component.html',
   styleUrls: ['./customcounter.component.scss']
 })
-export class CustomcounterComponent {
+export class CustomcounterComponent implements OnInit{
   constructor(private store: Store<{counter: CounterModel}>) {
 
   }
 
   counterInput!: number;
   actionType = "add";
+  channelName="";
+  counterSubscribe !: Subscription;
+  counter$ !: Observable<CounterModel>;
+  
 
+
+  ngOnInit(): void {
+    this.counterSubscribe = this.store.select(getChannelName).subscribe(data => {
+      this.channelName = data;
+      console.log('custom counter');
+    });
+    this.counter$ = this.store.select('counter');
+  }
   onCustomIncrement() {
     this.store.dispatch(customIncrement({ value: +this.counterInput , action:this.actionType }));
   }
